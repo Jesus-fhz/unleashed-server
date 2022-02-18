9280aa3bbe775f0e41be4edff88d11bf74e1078f
Laurence Qi <laurence.qi@outlook.com>
2022-02-18 23:19:48 +1100

    added notes to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,10 @@ class User < ApplicationRecord
 
   enum :user_type, [:walker, :owner]
 
+  #TODO: convert the address entered to a geocode. 
+
+  def address_to_geocode
+    # figure out how to update the values of the table row given the address. 
+    # TODO: even figure out how to make it a constructor method. 
+  end
 end
