3cf34ec2ea93e7852f5205180e8d0a3ce27f41a4
Laurence Qi <laurence.qi@outlook.com>
2022-02-19 00:07:01 +1100

    added distance code from previous project

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,12 @@ class User < ApplicationRecord
 
   #TODO: convert the address entered to a geocode. 
 
-  def address_to_geocode
+  def self.address_to_geocode address
+    data = Geocoder.search(address).first.data
+    lat = data.["lat"].to_f
+    lng = data.["lng"].to_f
+    
+    
     # figure out how to update the values of the table row given the address. 
     # TODO: even figure out how to make it a constructor method. 
   end
