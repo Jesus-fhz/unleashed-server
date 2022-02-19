df46049ead4a61150f3a97d25011fed479c7cc73
Laurence Qi <laurence.qi@outlook.com>
2022-02-19 18:50:19 +1100

    fixed the issues wit hthe show_nearby_walkers issue

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   post 'user_token' => 'user_token#create'
   get '/users/:id/pets' => 'users#show_owned_pets'
-  get '/users/find/:lat/:lng' => 'users#show_nearby_walkers'
+  get '/users/find/:lat/:lng' => 'users#show_nearby_walkers', constraints: { lat: /.*/, lng: /.*/ }
   get '/users/:id/walks' => 'users#show_user_walks'
 
   get '/pets/:id/walks' => 'pets#show_pet_walks'
