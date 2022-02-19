40c4ac5f0534c026474869e3110571662a36cbe0
Daniel Lord-Doyle <dlorddoyle@gmail.com>
2022-02-19 16:17:29 +1100

    updated all controllers for before actions and ensured all could communicate with the react front end via json with headers

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
 
   get '/users/:id/pets' => 'users#show_owned_pets'
   get '/users/find/:lat/:lng' => 'users#show_nearby_walkers'
+  get '/users/:id/walks' => 'users#show_user_walks'
+
+  get '/pets/:id/walks' => 'pets#show_pet_walks'
   
   resources :pets
   resources :users
