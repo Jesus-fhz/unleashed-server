9b28749fa5b97b1702015657112273d1c2e36eab
Laurence Qi <laurence.qi@outlook.com>
2022-02-19 18:18:50 +1100

    removed the add email and password migration

--- db/migrate/20220219060614_add_email_and_password_digest_to_users.rb
+++
@@ -1,6 +0,0 @@
-class AddEmailAndPasswordDigestToUsers < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :users, :email, :text
-    remove_column :users, :password_digest, :text
-  end
-end
