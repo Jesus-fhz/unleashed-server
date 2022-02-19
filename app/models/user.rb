0bc9c3d3b99882d592a3c5bec1a08ce2c4b2edc7
Laurence Qi <laurence.qi@outlook.com>
2022-02-19 12:39:59 +1100

    fixed error

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,14 +9,15 @@ class User < ApplicationRecord
   # FIXME: Ask Luke why the line below line does not work as intended. It should refer to usertypes variable above and validate entries on form submit. It just seems to make the DB not accept either owner or walker as an option anymore.
   # validates :user_type, inclusion: { in: User::USERTYPES }
 
+  
   #TODO: convert the address entered to a geocode. 
-
-  # def self.address_to_geocode address
-  #   data = Geocoder.search(address).first.data
-  #   lat = data.["lat"].to_f
-  #   lng = data.["lng"].to_f
+  def self.address_to_geocode address
+    data = Geocoder.search(address).first.data
     
-  #   # figure out how to update the values of the table row given the address. 
-  #   # TODO: even figure out how to make it a constructor method. 
-  # end
+    lat = data["lat"].to_f
+    lng = data["lng"].to_f
+    
+    # figure out how to update the values of the table row given the address. 
+    # TODO: even figure out how to make it a constructor method. 
+  end
 end
