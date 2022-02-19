122f761f10933cb5ff00f4d54f4e4c7c5fa98ed3
Laurence Qi <laurence.qi@outlook.com>
2022-02-19 13:13:41 +1100

    added show_nearby_walkers route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
 
   get '/users/:id/pets' => 'users#show_owned_pets'
-
+  get '/users/find/:lat/:lng' => 'users#show_nearby_walkers'
+  
   resources :pets
   resources :users
   resources :walks
