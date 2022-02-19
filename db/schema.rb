f6aced621251c443ab881463185b301782cba200
Daniel Lord-Doyle <dlorddoyle@gmail.com>
2022-02-19 11:25:03 +1100

    added default value to users type

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_18_215859) do
+ActiveRecord::Schema.define(version: 2022_02_19_002222) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
