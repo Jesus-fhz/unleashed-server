72eac49f13525d8a6ac42a767f00d13fb1361cf3
Jesus-fhz <jesusflores9325@gmail.com>
2022-02-27 20:41:41 +1100

    secret env phrase for deployment

--- config/initializers/knock.rb
+++ config/initializers/knock.rb
@@ -2,7 +2,7 @@ Knock.setup do |config|
   
   config.token_lifetime = 1.day
   config.token_signature_algorithm = 'HS256'
-  config.token_secret_signature_key = -> { ENV["UNLEASHED_SECRET_PHRASE"] }
+  config.token_secret_signature_key = -> {ENV["UNLEASHED_SECRET_PHRASE"]}
   config.not_found_exception_class_name = 'ActiveRecord::RecordNotFound'
   
 end
